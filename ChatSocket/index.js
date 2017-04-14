5062700557f89c9c6862e4b1ad5a71ae709cf518
blueoyw <blueoyw@naver.com>
2017-04-14 18:12:34 +0900

    first commit

---
+++ ChatSocket/index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3001;
+
+/*
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');	
+});
+*/
+
+io.on('connection', function(socket){  //conneted call back
+	console.log('connected');
+  socket.on('chat message', function(msg){  //chat message call back
+	console.log( msg );
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+	  console.log('disconnected');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
