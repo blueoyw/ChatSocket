fc3762087ae9716d6b8cd8da215eff20a8aa24bb
blueoyw <blueoyw@naver.com>
2017-04-17 10:45:54 +0900

    add redis subscibe

--- ChatSocket/index.js
+++ ChatSocket/index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis')
 var port = process.env.PORT || 3001;
 
 /*
@@ -9,17 +10,42 @@ app.get('/', function(req, res){
 });
 */
 
-io.on('connection', function(socket){  //conneted call back
-	console.log('connected');
-  socket.on('chat message', function(msg){  //chat message call back
-	console.log( msg );
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-	  console.log('disconnected');
-  });
+//redis
+var client = redis.createClient(6379, "119.194.139.163", {
+	password : "redis!"
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+io.on('connection', function(socket) { //conneted call back
+	console.log('connected');
+		
+	socket.on('disconnect', function() {
+		console.log('disconnected');		
+	});
+	
+	/*
+	socket.on('chat message', function(msg) { //chat message call back
+		console.log(msg);
+		io.emit('chat message', msg);
+	});
+	*/	
 });
+
+//redis message
+client.on("message", function( channel, message ){
+	console.log("sub channel " + channel + ": " + message);	    	
+	io.emit('chat message', message);
+	/*
+    sub.unsubscribe();
+    sub.quit();
+    pub.quit();
+    */	    
+});		
+
+http.listen(port, function() {
+	console.log('listening on *:' + port);
+});
+
+//redis subscribe
+client.subscribe("chat");
+//client.unsubscribe();
+//client.quit();
